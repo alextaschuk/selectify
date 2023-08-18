0277dc59ec742916b7a290fc7bde783b87873370
ataschuk <alextaschuk@gmail.com>
2023-08-18 00:30:46 -0400

    Fixed flask host ip

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def refresh_token():
         return 'Could not generate new access token.'
 
 if __name__ == '__main__':
-    app.run(host='10.0.0.130', port=5000, debug=True)
+    app.run(host='10.0.0.248', port=5000)
